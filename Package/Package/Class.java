1ec265c78c53518c80e7e18ca284eb2eb257a3a7
daffatahtaa <43267539+daffatahtaa@users.noreply.github.com>
2018-11-27 21:58:57 +0700

    pembuatan design awal

---
+++ Package/Package/Class.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Package;
+
+/**
+ *
+ * @author Expression daffa is undefined on line 12, column 14 in Templates/Classes/Class.java.
+ */
+public class Class {
+
+    public Class(String jumlah) {
+        
+    }
+    
+            
+}
