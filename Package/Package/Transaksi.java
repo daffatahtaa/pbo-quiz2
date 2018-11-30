e2084c3ceb25857741faed8869cf34d8f1d971a6
daffatahtaa <43267539+daffatahtaa@users.noreply.github.com>
2018-11-30 20:35:49 +0700

    Pembuatan Class transaksi
    Pembuatan Class transaksi yang di gunakan untuk menghandle transaksi

---
+++ Package/Package/Transaksi.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Package;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Expression daffa is undefined on line 12, column 14 in Templates/Classes/Class.java.
+ */
+public class Transaksi {
+    private String kode;
+    private ArrayList<ItemArray> item = new ArrayList();
+    
+    private float total;
+    
+    public Transaksi(){
+        this.kode = null;
+    }
+
+    public Transaksi(String kode, float total) {
+        this.kode = kode;
+        this.total = total;
+    }
+    public void hitungTotal(){
+        float total = 0;
+        for (ItemArray item : this.item) {
+            total += item.total();
+        }
+        this.total = total;
+    }
+    
+    public String Pembayaran(){
+        hitungTotal();
+        String ss = "";
+        ss += "Kode\t\t : " + this.kode + "\n";
+        ss += "Daftar belanja : \n";
+        for (ItemArray item : this.item) {
+            ss += "\t" + item.getNama()+ "(" + item.getJumlah() + ")" + item.getJumlah() + "\n";
+        }
+        ss += "Total \t\t : " + this.total;
+        return ss;
+    }
+}
